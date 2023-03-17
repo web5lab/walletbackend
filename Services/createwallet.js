2fcb490a81ef78d44010a9b1875bfe1b338d0b00
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2023-03-17 16:51:17 +0530

    extended to btc testnet

--- Services/createwallet.js
+++ Services/createwallet.js
@@ -6,8 +6,19 @@ const seed = bitcoinWalletNode.crypto.sha256(MasterPhrase);
 const ecc = require('tiny-secp256k1')
 const { BIP32Factory } = require('bip32');
 const bip32 = BIP32Factory(ecc)
-const bitcoinMasterKey = bip32.fromSeed(seed)
+const bitcoinMasterKey = bip32.fromSeed(seed,bitcoinWalletNode.networks.testnet)
+const bs58 = require('bs58');
 
+
+const bytes = Uint8Array.from([
+    0, 60,  23, 110, 101, 155, 234,
+   15, 41, 163, 233, 191, 120, 128,
+  193, 18, 177, 179,  27,  77, 200,
+   38, 38, 129, 135
+])
+console.log(bytes)
+const address = bs58.encode(bytes)
+console.log(address)
 
 // get multiple wallet in single click
 const createMultipleNewWallet = (n) => {
@@ -15,7 +26,7 @@ const createMultipleNewWallet = (n) => {
     for (let i = 1; i <= n; i++) {
       const child = bitcoinMasterKey.derivePath(`m/0/${i}`)
       const publicKey = child.publicKey;
-      const { address } = bitcoinWalletNode.payments.p2pkh({ pubkey: publicKey });
+      const { address , privateKey} = bitcoinWalletNode.payments.p2pkh({ pubkey: publicKey ,network:bitcoinWalletNode.networks.testnet});
       const obj = {
         Userid: i,
         BtcAdrress:`your bitcoin wallet adress is ${address}`,
@@ -31,12 +42,12 @@ const createMultipleNewWallet = (n) => {
 const getWallet = (Userid)  => {
   const child = bitcoinMasterKey.derivePath(`m/0/${Userid}`)
   const publicKey = child.publicKey;
-  const privateKey = child.privateKey.buffer;
-  const { address} = bitcoinWalletNode.payments.p2pkh({ pubkey: publicKey });
+  const privateKey = child.toWIF();
+  const { address} = bitcoinWalletNode.payments.p2pkh({ pubkey: publicKey , network:bitcoinWalletNode.networks.testnet});
   const obj = {
     Userid:Userid,
     BtcAdrress:address,
-    BtcPrivateKey:privateKey||"wallet key not availabel",
+    BtcPrivateKey:privateKey,
     EthAdress:walletNode.derivePath(`m/44'/60'/0'/0/${Userid}`).address
   };
  return obj
