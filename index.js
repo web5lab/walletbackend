29c12c17c174ec156c057d3df975ef75e2aa82f7
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2023-03-30 15:28:28 +0530

    up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {VerifyAdmin} = require('./middleware/adminAuth')
 const {VerifyServer} = require('./middleware/serverAuth')
 const bodyParser = require('body-parser');
 const databaseConnection = require('./mongoDb/db')
-const cors = require('cros');
+const cors = require('cors');
 
 
 const app = express()
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Add the error log middleware to the app
 app.use(logErrors);
-app.use(cors('*'))
+app.use(cors({origin:'*'}))
 
 
 
