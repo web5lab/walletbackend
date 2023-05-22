db9c26e113340f7b08db3e29dc5ef0df8a6bbb11
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2023-05-22 16:55:21 +0530

    up

--- Services/testnet/userService.js
+++ Services/testnet/userService.js
@@ -265,7 +265,7 @@ const getUserData = async (userId) => {
     const userCurrency = {
       icon: currency.icon,
       symbol: currency.currencyName,
-      balance: getUserCurrencyBalance(user, currency.currencyName).toFixed(8).toString(),
+      balance: getUserCurrencyBalance(user, currency.currencyName).toFixed(8),
       lotteryCurrency: currency.lotteryCurrency,
       currencyType: "CRYPTO",
       currencyId: currency._id,
